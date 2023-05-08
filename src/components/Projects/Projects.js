5cbf5a5de200f8c87d296500995f0ca52ceabcfa
Victor <maragiogliov@protonmail.com>
2023-05-08 21:37:12 +0200

    go

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,10 +6,35 @@ import './Projects.css';
 export default function Projects() {
   return (
       <>
+      <h4 className='projects-collaborations'>Collaborations </h4>
+        <h1 className='projects-recent'>Recent Projects</h1>
       <div className='projects-container'>
-        <h1>Projects
-          
-        </h1>
+
+        <div className='project-card-container'>
+        <h1 className='projects-title'>MOTHERCODE</h1>
+        <h4 className='projects-title-description'>Final Project DCI</h4>
+
+        <h1 className='projects-title'>CDE OLIVE OIL</h1>
+        <h4 className='projects-title-description'>Olive Oil Website using React.js </h4>
+
+        <h1 className='projects-title'>NED</h1>
+        <h4 className='projects-title-description'>Frontend development for a digital art gallery using React.js</h4>
+
+
+        <h1 className='projects-title'>LENGUAPP</h1>
+        <h4 className='projects-title-description'>UX Research </h4>
+
+
+        <h1 className='projects-title'>CV Desserts</h1>
+        <h4 className='projects-title-description'>Shopify E-commerce </h4>
+
+
+        <h1 className='projects-title'>Wheather App</h1>
+        <h4 className='projects-title-description'>React.js project </h4>
+
+        <h1 className='projects-title'>Guess Game</h1>
+        <h4 className='projects-title-description'>Vanilla JavaScript </h4>
+        </div>
       </div>
       </>
     )
